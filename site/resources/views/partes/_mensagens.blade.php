12c64e7245cf3f9dd9230dcbd663452474a776a4
Alex <alex.cc20@hotmail.com>
2017-03-22 13:40:06 -0300

    correção de rotas auth, enibindo a rota register

--- site/resources/views/partes/_mensagens.blade.php
+++ site/resources/views/partes/_mensagens.blade.php
@@ -8,11 +8,13 @@
 
 @if (count($errors) >0)
 	<div class="alert alert-danger" role="alert">
-		<strong>Voce cometeu um erro </strong>
+		<strong>Você cometeu um erro </strong>{{Session::get('danger')}}
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+			<div class="alert alert-success">{{ Session::get('flash_message') }}</div>
 		<ul>
 		@foreach ($errors->all() as $error)
 
-			<li> {{'1 - '.$error}}</li>
+			<li> {{'* - '.$error}}</li>
 		@endforeach
 		</ul>
 	</div>
