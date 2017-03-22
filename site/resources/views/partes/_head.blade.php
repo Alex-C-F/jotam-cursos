eeedbd5ae9a7d85b24b90cf63376e01f833e4b56
Alex <alex.cc20@hotmail.com>
2017-03-22 19:18:10 -0300

    inserção de favicon em head

--- site/resources/views/partes/_head.blade.php
+++ site/resources/views/partes/_head.blade.php
@@ -4,12 +4,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Jotam Cursos @yield('title')</title><!--mudar o texto dessa página-->
-
+    <link rel="shortcut icon" href= "{{asset('favicon.ico')}}"" type="image/x-icon">
     <!-- Bootstrap -->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="http://cdn.bootcss.com/animate.css/3.5.1/animate.min.css">
+
 
    {{ Html::style('css/styles.css') }}
    @yield('stylesheet')
